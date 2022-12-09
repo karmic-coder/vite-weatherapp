686c8024ddecaf2ceef3210935b0084a9ca53b91
karmic-coder <karmic-coder@karmatronix.com>
2022-12-09 09:27:52 -0500

    fixed error handling for when NWS server lookups fail

--- src/Forecast.jsx
+++ src/Forecast.jsx
@@ -3,6 +3,37 @@ import React from "react";
 import { useState, useEffect } from "react";
 import DailyWeather from "./DailyWeather";
 
+const ForecastError = ({ forecastError }) => {
+  const [timer, setTimer] = useState(5);
+  function myTimer() {
+    var timeleft = 5;
+    var downloadTimer = setInterval(function () {
+      if (timeleft <= 0) {
+        clearInterval(downloadTimer);
+        // getForecast();
+        console.log(timer);
+      } else {
+        // console.log(timeleft);
+        setTimer(timeleft);
+      }
+      timeleft -= 1;
+    }, 1000);
+  }
+  useEffect(() => {
+    myTimer();
+  }, []);
+  if (timer > 1) {
+    return (
+      <div className="forecast-error">
+        <h3>{forecastError}</h3>
+        <p>Trying again in {timer} seconds</p>
+      </div>
+    );
+  } else {
+    return;
+  }
+};
+
 const Forecast = (props) => {
   // console.log(props);
   const { city, state } = props.props.properties.relativeLocation.properties;
@@ -13,14 +44,26 @@ const Forecast = (props) => {
   };
 
   const [localWeather, setLocalWeather] = useState({});
+  const [forecastError, setForecastError] = useState("");
 
   const getForecast = async () => {
-    const result = await fetch(`${forecast}`);
-    const data = await result.json();
+    try {
+      const result = await fetch(`${forecast}`);
+      const data = await result.json();
+      // const data = {};
+      // data.status = 500; // force trigger error
+      if (data.status || data.status == 500) {
+        setForecastError("National Weather Service - server error");
+      }
+      setLocalWeather(data);
+    } catch (error) {
+      console.error("Forecast error: " + error);
+      setForecastError(error);
+      console.log("Fc error:", forecastError);
+    }
     // console.log(result);
     // console.log(data);
     // return data;
-    setLocalWeather(data);
   };
   useEffect(() => {
     getForecast();
@@ -38,8 +81,16 @@ const Forecast = (props) => {
   //   return icon;
   // });
   // console.log(day);
+  if (!isEmpty(forecastError)) {
+    setTimeout(() => {
+      // console.log(localWeather);
+      getForecast();
+      setForecastError({});
+    }, 6000);
+    return <ForecastError forecastError={forecastError} />;
+  }
 
-  if (!isEmpty(localWeather)) {
+  if (!isEmpty(localWeather) && localWeather.status !== 500) {
     const dailyDetails = localWeather.properties.periods;
     return (
       <>
